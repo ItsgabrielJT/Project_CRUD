dcd83aab4d379346c96097557cb93c0d44b52e68
Itsgabriel <gabrielasimbaya82@gmail.com>
2023-01-04 16:57:21 -0500

    Se implmento templates en todas las vistas

---
+++ resources/views/templates/index.blade.php
@@ -0,0 +1,26 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            @yield('title')
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @yield('button-add')
+        </div>        
+
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+            @if (session('status'))            
+                <x-session/>
+            @endif
+
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <!-- component Table-->
+                @yield('table')
+            </div>
+        </div>
+    </div>
+</x-app-layout>
